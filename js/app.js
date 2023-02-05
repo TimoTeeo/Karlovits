fbd9f8b100a485e7205901d80985adf526a41643
DESKTOP-1KR2NBI\timos <t.diakakis@pobuca.com>
2023-02-05 03:16:02 +0200

    4/2 changes

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@ const header = document.querySelector('.header');
 const menuBurger = document.getElementById('header-menu');
 const dropdownMenu = document.querySelector('.header__menu--dropdown');
 const workLink = document.querySelector('.work-link');
+const photographyLink = document.querySelector('.photography-link');
 const dropdownMenuWork = document.querySelector('.header__menu--dropdown--work-nested');
-const backArrow = document.querySelector('.arrow-container img');
+const dropdownMenuPhotography = document.querySelector('.header__menu--dropdown--photography-nested');
+const backArrow = document.querySelectorAll('.arrow-container img');
 
 console.log(dropdownMenu);
 window.addEventListener('scroll', function() {
@@ -20,6 +22,9 @@ menuBurger.addEventListener('click', function() {
     if (dropdownMenuWork.classList.contains('show')) {
         dropdownMenuWork.classList.remove('show');
     }
+    if (dropdownMenuPhotography.classList.contains('show')) {
+        dropdownMenuPhotography.classList.remove('show');
+    }
     console.log(dropdownMenu);
     menuBurger.classList.toggle('change');
     dropdownMenu.classList.toggle('show');
@@ -32,10 +37,19 @@ workLink.addEventListener('click', function(e) {
     menuBurger.classList.add('change');
     dropdownMenuWork.classList.add('show')
 })
+// photographyLink.addEventListener('click', function(e) {
+//     console.log(e);
+//     e.stopPropagation();
+//     menuBurger.classList.add('change');
+//     dropdownMenuPhotography.classList.add('show')
+// })
 
-backArrow.addEventListener('click', function(e) {
-    e.stopPropagation();
-    // menuBurger.classList.add('change');
-    dropdownMenuWork.classList.remove('show');
-    dropdownMenu.classList.add('show');
-})
+backArrow.forEach((arrow) => {
+    arrow.addEventListener('click', function(e) {
+        e.stopPropagation();
+        // menuBurger.classList.add('change');
+        dropdownMenuWork.classList.remove('show');
+        dropdownMenuPhotography.classList.remove('show');
+        dropdownMenu.classList.add('show');
+    })
+}) 
